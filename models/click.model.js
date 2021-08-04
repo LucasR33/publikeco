d40470bb06ca0cf13ad672ec766a6b3716d66296
elaforest <laforestetienne@live.fr>
2021-08-04 18:53:16 +0200

    Update click.model.js

--- models/click.model.js
+++ models/click.model.js
@@ -1,1 +1,26 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 
+const ClickSchema = new Schema({
+    id_joueur: {
+    type: Number
+  },
+  datetime_clic: {
+    type: Date,
+    required: true
+  },
+  id_annonceur: {
+    type: Number,
+    required: true
+  },
+  id_emplacement_annonce: {
+    data: Number,
+    required: true
+  },
+  id_jeu: {
+    type: Number,
+    required: true
+  },
+});
+
+module.exports = Click = mongoose.model('Click', ClickSchema);
