da34573cf14e7ec8496af17cf41bad6c47af5ee9
Julien Perrin <julien.perrin@viacesi.fr>
2021-08-26 20:58:00 +0200

    validation champs enchères

--- controllers/enchere.controller.js
+++ controllers/enchere.controller.js
@@ -2,40 +2,57 @@ const enchere = require('../models/enchere.model');
 const annonce = require('../models/annonce.model');
 const compte = require('../models/compte.model');
 const jeux = require('../models/jeux.model');
+const { body, validationResult } = require('express-validator');
 var ObjectId = require('mongodb').ObjectID;
 
 const async = require('async');
 
-
 exports.create = (req, res) => {
-    // let idAnnoArray = [];
-    let idJeu = req.body.nom_jeux;
-    let test = idJeu;
-    var hex = /[0-9A-Fa-f]{6}/g;
-    test = (hex.test(test))? ObjectId(test) : test;
-    jeux.findOne({'_id': ObjectId(test)}, function(err, game) {
-        if(err){
-            console.log(err);
-        }else {
-            let list = req.body.libelle.map(e => ObjectId(e));
-            annonce.find({"_id":{"$in":list}}, (err, ann) => {
-                ann = list.map(e => ann.find(s => s._id.equals(e)));
-                const newEnchere = new enchere({
-                    datetime_enchere: new Date(),
-                    jeu: game,
-                    annonces: ann,
-                    emplacement_annonce: req.body.emp,
-                    prix_vainqueur: 0,
-                    done: false
-                })
-                newEnchere.save().then(data => res.redirect('listEnchere'));
-                
+    const errors = validationResult(req);
+    if(errors.isEmpty()) {
+        // let idAnnoArray = [];
+        let idJeu = req.body.nom_jeux;
+        let test = idJeu;
+        var hex = /[0-9A-Fa-f]{6}/g;
+        test = (hex.test(test))? ObjectId(test) : test;
+        jeux.findOne({'_id': ObjectId(test)}, function(err, game) {
+            if(err){
+                console.log(err);
+            }else {
+                let list = req.body.libelle.map(e => ObjectId(e));
+                annonce.find({"_id":{"$in":list}}, (err, ann) => {
+                    ann = list.map(e => ann.find(s => s._id.equals(e)));
+                    const newEnchere = new enchere({
+                        datetime_enchere: new Date(),
+                        jeu: game,
+                        annonces: ann,
+                        emplacement_annonce: req.body.emp,
+                        prix_vainqueur: 0,
+                        done: false
+                    })
+                    newEnchere.save().then(data => res.redirect('listEnchere'));
                 });
-        }
-    });
+            }
+        })
+    } else {
+        annonce.find({}, function(err, annonces){
+            if(err){
+                console.log(err);
+            }else {
+                jeux.find({}, function(err, jeux){
+                    if(err){
+                        console.log(err);
+                    }else {
+                        res.render('enchere/addEnchere', {jeux:jeux, annonces:annonces, errors: errors.array()});
+                    }
+                });
+            }
+        });
+    }
 };
 
 exports.addEnchere = (req, res) => {
+    const errors = undefined;
     annonce.find({}, function(err, annonces){
         if(err){
             console.log(err);
@@ -44,7 +61,7 @@ exports.addEnchere = (req, res) => {
                 if(err){
                     console.log(err);
                 }else {
-                    res.render('enchere/addEnchere', {jeux:jeux, annonces:annonces});
+                    res.render('enchere/addEnchere', {jeux:jeux, annonces:annonces, errors: errors});
                 }
             });
         }
--- routes/enchere.routes.js
+++ routes/enchere.routes.js
@@ -1,7 +1,12 @@
+const { body, validationResult } = require('express-validator');
 module.exports = (app) => {
     const enchere = require('../controllers/enchere.controller.js');
 
-    app.post('/enchere/addEnchere', enchere.create);
+    app.post(
+        '/enchere/addEnchere',
+        body("libelle").notEmpty(),
+        enchere.create
+    );
 
     app.get('/enchere/addEnchere', enchere.addEnchere);
 
