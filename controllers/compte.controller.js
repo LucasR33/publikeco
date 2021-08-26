e71aecdcfaa447efd28342a8ae1d29982e36fd97
Julien Perrin <julien.perrin@viacesi.fr>
2021-08-26 13:39:37 +0200

    validation champs à la création d'un compte

--- controllers/compte.controller.js
+++ controllers/compte.controller.js
@@ -1,24 +1,31 @@
 const compte = require('../models/compte.model');
+const { body, validationResult } = require('express-validator');
 //TO-DO faire les updates annonceurs et editeurs
 
 exports.create = (req, res) => {
-    const newCompte = new Compte({
-        nom:req.body.nom,
-        prenom:req.body.prenom,
-        mdp:req.body.mdp,
-        email:req.body.email,
-        type:req.body.type
-    });
-    newCompte.save().then(data => res.redirect('listCompte'));
+    const errors = validationResult(req);
+    if (errors.isEmpty()) {
+        // Création compte
+        const newCompte = new Compte({
+            nom: req.body.nom,
+            prenom: req.body.prenom,
+            mdp: req.body.mdp,
+            email: req.body.email,
+            type: req.body.type
+        });
+        newCompte.save().then(data => res.redirect('listCompte'));
+    } else {
+        res.render('compte/addCompte', { errors: errors.array() });
+    }
 };
 
 exports.addCompte = (req, res) => {
-    res.render('compte/addCompte');
+    const errors = undefined
+    res.render('compte/addCompte', { errors: errors });
 };
 
 exports.findAll = (req, res) => {
     compte.find().then(data => {
-        res.render('compte/listCompte', {compte:data});
+        res.render('compte/listCompte', { compte: data });
     });
 };
-
